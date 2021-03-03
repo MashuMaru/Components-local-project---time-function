b23fe59f91faab2c6a14d5a6a16d6ad0238c725a
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2021-03-03 07:07:52 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <TheHeader />
+    <BadgeList />
+    <UserInfo
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    />
+    <p>
+      <span>{{ hour }} </span>
+    </p>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader";
+import BadgeList from "./components/BadgeList";
+import UserInfo from "./components/UserInfo";
+
+export default {
+  components: { TheHeader, BadgeList, UserInfo },
+  data() {
+    return {
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+      hour: "",
+      // minutes: minutes,
+    };
+  },
+  methods: {
+    getHours() {
+      this.hour = setInterval(() => {
+        var now = new Date();
+        var hour = now.getHours();
+        var minutes = now.getMinutes();
+        var seconds = now.getSeconds();
+        return (this.hour = hour + " : " + minutes + " : " + seconds);
+      });
+      // var now = new Date();
+      // var hour = now.getHours();
+      // var minutes = now.getMinutes();
+      // var seconds = now.getSeconds();
+      // return (this.hour = hour + " : " + minutes + " : " + seconds);
+    },
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
